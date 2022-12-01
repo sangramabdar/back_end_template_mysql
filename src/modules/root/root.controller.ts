19df4ec28a746ede620abc8477f9f5183dfbc287
sangramabdar <abdarsangram2697@gmail.com>
2022-12-01 14:07:47 +0530

    updated

--- src/modules/root/root.controller.ts
+++ src/modules/root/root.controller.ts
@@ -4,17 +4,6 @@ class RootController {
   static async get(req: Request, res: Response) {
     return res.send("app");
   }
-
-  static async wrongRoute(req: Request, res: Response) {
-    console.log("wrong route");
-    return res.sendStatus(404);
-  }
-
-  static async successRoute(req: Request, res: Response) {
-    return res.json({
-      result: "success",
-    });
-  }
 }
 
 export default RootController;
